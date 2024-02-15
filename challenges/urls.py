0391b118f6976f3981f0c551efa55f1af6f9a4de
s4remi <s4remi@gmail.com>
2024-02-14 20:10:50 -0800

    adding dynamic segments & capture values

--- challenges/urls.py
+++ challenges/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("february", views.february), path("march", views.march)]
+urlpatterns = [
+    # path("february", views.february),
+    # path("march", views.march),
+    path("<month>", views.monthly_challenge),
+]
--- challenges/views.py
+++ challenges/views.py
@@ -3,9 +3,20 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def february(request):
-    return HttpResponse("this challenge is created for Feb via Ali Sarmei!")
+# def february(request):
+#     return HttpResponse("this challenge is created for Feb via Ali Sarmei!")
 
 
-def march(request):
-    return HttpResponse("this challenge is created for March via  Ali Sarmei.")
+# def march(request):
+#     return HttpResponse("this challenge is created for March via  Ali Sarmei.")
+
+
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == "february":
+        challenge_text = "Feb Challenge"
+    elif month == "march":
+        challenge_text = "March Challenge"
+    else:
+        return HttpResponse("<h1>Invalid Month</h1><p>Please enter a valid month.</p>")
+    return HttpResponse(challenge_text)
