e3e7c4b5ff262759112beeeb6fe0e94e11ef1314
s4remi <s4remi@gmail.com>
2024-02-14 19:57:14 -0800

    add march view to webpage

--- challenges/urls.py
+++ challenges/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("february", views.index)]
+urlpatterns = [path("february", views.february), path("march", views.march)]
--- challenges/views.py
+++ challenges/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+def february(request):
+    return HttpResponse("this challenge is created for Feb via Ali Sarmei!")
 
 
-def index(request):
-    return HttpResponse("this is Ali Sarmei!")
+def march(request):
+    return HttpResponse("this challenge is created for March via  Ali Sarmei.")
