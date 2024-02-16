01e4b252e70e9ec4186e1a3ae290d05d8472e747
s4remi <s4remi@gmail.com>
2024-02-15 16:09:03 -0800

    option 1 to add html file to monthly-challenge in view

--- montly_challenges/settings.py
+++ montly_challenges/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "montly_challenges.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "challenges" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
