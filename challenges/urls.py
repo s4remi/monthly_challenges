e19525543e0b61c758766ac1af82599c7458824e
s4remi <s4remi@gmail.com>
2024-02-16 21:07:21 -0800

    use include tag for having a nav bar in html pages

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),  # to saee the list of the month in /challenges
+    path("", views.index, name="index"),  # to see the list of the month in /challenges
     path("<int:month>", views.monthly_number_challenge),
     # adding key name to make sure we are able to have a dynamic url path instead of
     # hard coding it in the views.py
